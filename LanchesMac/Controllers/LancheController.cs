f9733a2fde0c8a79a382a6468afdc6b740deacda
pedro.santos <pedro.santos@sistemafibra.org.br>
2024-02-27 15:08:34 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,35 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewData["Titulo"] = "Todos os Lanches";
+            //ViewData["Data"] = DateTime.Now;
+
+            //var lanches = _lancheRepository.Lanches;
+            //var totalLanches = lanches.Count();
+
+            //ViewBag.Total = "Total de lanches : ";
+            //ViewBag.TotalLanches = totalLanches;
+
+            //return View(lanches);
+
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lanchesListViewModel);
+        }
+    }
+}
