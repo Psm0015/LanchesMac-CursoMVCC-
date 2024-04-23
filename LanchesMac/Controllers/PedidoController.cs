c3f84fd53d7308cac65a07131d0e5048d1e36512
Pedro Mota <pedro.ppssmm@gmail.com>
2024-04-22 22:42:16 -0300

    Aula 72

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -15,15 +15,54 @@ namespace LanchesMac.Controllers
             _pedidoRepository = pedidoRepository;
             _carrinhoCompra = carrinhoCompra;
         }
-
+        [HttpGet]
         public IActionResult Checkout()
         {
             return View();
         }
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche...");
+            }
+
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if(ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+
+            }
+            return View(pedido);
+
+        }
+
+        public ActionResult CheckoutCompleto()
+        {
+            return View();
+        }
     }
 }
